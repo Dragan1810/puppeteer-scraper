b154678689d3fbb307d402de61b037603f2a5044
dragan1810 <d.novakovic1810@yahoo.com>
2019-01-03 17:45:19 +0100

    progeres

--- index.js
+++ index.js
@@ -21,23 +21,41 @@ function getUrl() {
   Divz.forEach(el => el.click());
 
   const DataBox = ".group-head.clickable.expanded.loaded";
-  const ContentBox = "even.expanded.first.last.match.no-date-repetition";
+  const ContentBox = ".even.expanded.first.last.match.no-date-repetition";
 
-  //const Content = await page.$$(DataBox);
-  console.log([...document.querySelectorAll(ContentBox)].length);
+  const Content = await page.$$(ContentBox);
+  console.log(Content.length);
+  const stuff = await page.evaluate(dataBox => {
+    const minute = document
+      .querySelector(".minute")
+      .textContent.replace(/(\r\s\n\t|\n|\r\t)/gm, "")
+      .trim();
+    const teamA = document
+      .querySelector(".team.team-a > a")
+      .textContent.replace(/(\r\s\n\t|\n|\r\t)/gm, "")
+      .trim();
+    const score = document
+      .querySelector(".score-time.score > a")
+      .textContent.replace(/(\r\s\n\t|\n|\r\t)/gm, "")
+      .trim();
+    const teamB = document
+      .querySelector(".team.team-b > a")
+      .textContent.replace(/(\r\s\n\t|\n|\r\t)/gm, "")
+      .trim();
 
+    return { minute, teamA, teamB, score };
+  }, Content[0]);
+  console.log(stuff);
   /*
   // Extract the results from the page.
   const links = await page.evaluate(resultsSelector => {
     const anchors = Array.from(document.querySelectorAll(resultsSelector));
     return anchors.map(anchor => {
-      const title = anchor.textContent.split('|')[0].trim();
+      const title = anchor.textContent.split("|")[0].trim();
       return `${title} - ${anchor.href}`;
     });
   }, resultsSelector);
-  console.log(links.join('\n'));
-  
-  */
-
+  console.log(links.join("\n"));
+*/
   //await browser.close();
 })();
