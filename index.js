ffcef11fb6d9dc997b411e83fa2cabdc4eb1bab5
dragan1810 <d.novakovic1810@yahoo.com>
2019-01-03 12:29:46 +0100

    qqq

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const puppeteer = require("puppeteer");
 
-const URL = "http://www.scoresway.com/?sport=home&page=matches&date=2018-12-17";
+const URL =
+  "http://www.scoresway.com/?sport=soccer&page=matches&date=2018-12-17";
+
+function getUrl() {
+  let sport = "soccer";
+  let page = "matches";
+  let date = "2018-12-17";
+
+  return URL;
+}
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(URL);
-  await page.screenshot({ path: "example.png" });
 
-  await browser.close();
+  const MatchesHeaders = ".group-head.clickable";
+  const Divz = await page.$$(MatchesHeaders);
+  Divz.forEach(el => el.click());
+
+  const DataBox = ".group-head.clickable.expanded.loaded";
+  const ContentBox = "even.expanded.first.last.match.no-date-repetition";
+
+  const X = await page
+    .waitForSelector(Content)
+    .catch(() => console.error("ERR"));
+  const Content = await page.$$(ContentBox);
+  console.log(X);
+
+  /*
+  // Extract the results from the page.
+  const links = await page.evaluate(resultsSelector => {
+    const anchors = Array.from(document.querySelectorAll(resultsSelector));
+    return anchors.map(anchor => {
+      const title = anchor.textContent.split('|')[0].trim();
+      return `${title} - ${anchor.href}`;
+    });
+  }, resultsSelector);
+  console.log(links.join('\n'));
+  
+  */
+
+  //await browser.close();
 })();
